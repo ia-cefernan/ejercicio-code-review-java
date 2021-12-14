844ed71492e21221cae2cc228dcf73b59c653d71
Carla Fernández <79536103+ia-cefernan@users.noreply.github.com>
2021-12-14 17:42:44 -0500

    Update Calculadora.java
    Cambio en la operación de división

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -24,12 +24,17 @@ public class Calculadora {
         return resul;
     }
 
-    public double divi(int num1, int num2) {
+    public double division(int num1, int num2) {
         //metodo para la division
         //double es para un valor con punto decimal
-        double resul = 0;
-        resul = num1 / num2;
-        return resul;
+        double result = 0;
+         try {
+            result = num1 / num2;
+        } catch (Exception e) {
+           throw new RuntimeException(e);
+        }
+       
+        return result;
     }
 
 
